11dd4c9d3c5e988a70584f4131bb1d26fb09bf3f
hayangnyaho <entahsayajugagaktau@gmail.com>
2025-07-11 16:57:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+function jawab(){
+    var input = document.getElementById("isi").value.toLowerCase();
+    console.log("pertanyaan awal",input)
+
+      if (input.includes("#")) {
+        // hapus semua tanda #
+        let teksBersih = input.replace(/#/g, "").trim();
+        console.log("mengubah ke aksara:", teksBersih);
+        mengubah(teksBersih, teksBersih);
+    } else {
+        // kirim ke pertanyaan
+        pertanyaan(input);
+    }
+    
+}
+
+function mengubah(nama,input){
+   var text= nama.toLowerCase().match(/ng|ny|[bcdfghjklmnpqrstvwxyz]?[aiueoé]|[bcdfghjklmnpqrstvwxyz]|[\s]/gi);
+   console.log(text)
+   for (let i = 0; i < text.length - 1; i++) {
+    if (text[i] === 'ny'& text[i] != " ") {
+        text[i] = text[i] + text[i + 1]; // Gabungkan ny + a
+        text.splice(i + 1, 1);         // Hapus elemen berikutnya
+    }
+    else if (text[i] === 'ng' && text[i + 1] != " ") {
+        text[i] = text[i] + text[i + 1]; // Gabungkan ny + a
+        text.splice(i + 1, 1);         // Hapus elemen berikutnya
+    }}
+
+   console.log(text)
+   aksara(text,input)
+
+}
+function aksara(nama, input){
+    fetch('latin_ke_aksara_sunda.json')
+    .then(response => response.json())
+    .then(data => {
+    console.log("Data aksara:", data);
+    var hasil = ""
+    for (let i = 0; i < nama.length; i++){
+        const aks = data.find(item => item.kode === nama[i]);
+        if (aks){
+            hasil += aks.aksara
+            console.log(nama[i],' =', aks.aksara);
+        }
+            
+        else{
+            console.log("gak ada")
+            hasil += " "
+        }
+    }
+    console.log({input},{hasil})
+    hasil_aksara(hasil,input)
+  })
+}
+
+function hasil_aksara(hasil, input){
+    var jawab = document.getElementById("jawab");
+    jawab.innerHTML= hasil
+}
+function nilai(input){
+    if (["apa", "kapan", "dimana", "siapa", "bagaimana"].includes(input)) return 3;
+    if (["bunyi","terbuat", "dibuat", "berasal", "alat", "makanan","angklung","kecapi","jawa","rumah","digunakan","karakteristik","contoh"].includes(input)) return 2;
+    return 1;
+}
+
+function pertanyaan(input){
+    console.log("masuk")
+    fetch("artikel.txt")
+    .then(res => res.text())
+    .then(teks => {
+        let kata_tanya = input.match(/\w+/g); // ambil semua kata
+        let kalimat = teks.split(".");
+        console.log("pertanyaan pecah",kata_tanya)
+        console.log(kalimat)
+        var score = 0
+        var score_akhir = 0
+        for (var i = 0; i < kalimat.length; i++){
+            console.log("cek",score_akhir)
+            for(var j =0; j < kata_tanya.length; j++){
+                if (kalimat[i].toLowerCase().includes(kata_tanya[j].toLowerCase())){
+                    score += nilai(kata_tanya[j])
+                    console.log[kata_tanya[j]]
+                }
+            }
+            if (score > score_akhir){
+                score_akhir=score
+                console.log("akhir",score_akhir)
+                var jawaban_akhir = kalimat[i]
+            }
+            score = 0
+        }
+        console.log(jawaban_akhir)
+        
+        var jawab = document.getElementById("jawab");
+        jawab.innerHTML= jawaban_akhir
+    })
+}
